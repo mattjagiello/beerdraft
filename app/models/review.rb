c3008fe11a32c57a0576301850d2605b10b48482
mattjagiello <mjagiello@gmail.com>
2020-03-03 15:56:41 -0600

    Added beer name and timestamps to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,9 @@ class Review < ApplicationRecord
     belongs_to :user
     belongs_to :beer
     validates :content, presence: true
+
+    def beer_name_from_id(number)
+        beer = Beer.find_by(id: number)
+        beer.name
+    end
 end
